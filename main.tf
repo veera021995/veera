5d7532fdad0025a8b1870d1fe494e43d128b36ba
veera021995 <124140767+veera021995@users.noreply.github.com>
2023-03-20 22:28:23 +0530

    Update and rename movi1 to main.tf

--- main.tf
+++ main.tf
@@ -33,3 +33,52 @@ resource "aws_instance" "two" {
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by veera server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "server-2"
+  }
+}
+
+resource "aws_security_group" "three" {
+  name = "elb-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "four" {
+  bucket = "raham0077552bucketterra"
+}
+
+resource "aws_iam_user" "five" {
+name = "rahamuser11" 
+}
+
+resource "aws_ebs_volume" "six" {
+ availability_zone = "ap-south-1b"
+  size = 40
+  tags = {
+    Name = "ebs-001"
+  }
+}
